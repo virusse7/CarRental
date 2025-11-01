45195291dbdf7863da93b7a5c288dd11e07b3afe
Daniel Wieczorek <danielwieczorek901@gmail.com>
2025-11-01 14:42:02 +0100

    chore(setup) added nuxt icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  pages: true,
+  css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
+  modules: ['@nuxt/icon'],
+  icon: {
+    mode: 'css',
+    cssLayer: 'base'
+  }
 })
