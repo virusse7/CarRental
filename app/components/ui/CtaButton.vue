d802178c6eab8918ee49412a766f9d8b13601c75
Daniel Wieczorek <danielwieczorek901@gmail.com>
2026-01-02 16:40:14 +0100

    feat(ui): created SelectButton and CtaButton

---
+++ app/components/ui/CtaButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+    },
+    icon: {
+        type: String,
+        default: ''
+    },
+    class: {
+        type: [String, Array, Object],
+        default: ''
+    }
+})
+
+</script>
+
+<template>
+    <button class="btn-primary flex justify-center items-center gap-1" :class="props.class">
+        <span>{{ title }}</span>
+        <Icon v-if="icon" :name="icon" size="14" />
+    </button>
+</template>
---
+++ app/components/ui/SelectButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+    }
+})
+
+</script>
+
+<template>
+    <button class="">
+        <span>{{ title }}</span>
+    </button>
+</template>
