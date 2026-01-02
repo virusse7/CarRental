97f8f3da971451171406e0d276e8b7f0b69b5769
Daniel Wieczorek <danielwieczorek901@gmail.com>
2026-01-02 16:41:37 +0100

    feat(ui): created tile component

---
+++ app/components/promotions/tiles/Tile.vue
@@ -0,0 +1,23 @@
+<script setup>
+import CtaButton from '~/components/ui/CtaButton.vue';
+
+const props = defineProps({
+    title: String,
+    image: String,
+    description: String,
+    buttonText: String,
+    class: {
+        type: [String, Array, Object],
+        default: ''
+    }
+})
+</script>
+
+<template>
+    <div class="bg-cover bg-center w-full rounded-lg flex flex-col gap-1 justify-center p-2" :class="props.class"
+        :style="{ backgroundImage: image ? `url(${image})` : 'none' }">
+        <span class="text-header! text-xl">{{ title }}</span>
+        <p v-if="description">{{ description }}</p>
+        <CtaButton class="max-w-[180px]" v-if="buttonText" icon="ooui:arrow-next-ltr" :title="`${buttonText}`" />
+    </div>
+</template>
