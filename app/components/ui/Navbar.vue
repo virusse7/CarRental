428e8f29bbdd7e91ce280c31929d5cdc97c228eb
Daniel Wieczorek <danielwieczorek901@gmail.com>
2026-01-02 16:22:16 +0100

    feat(Navbar): created navbar component using NuxtUi

--- app/components/ui/Navbar.vue
+++ app/components/ui/Navbar.vue
@@ -1,16 +1,36 @@
+<script setup>
+
+const mobileItems = [
+    {
+        label: 'Menu',
+        icon: 'ci:hamburger',
+        children: [
+            { label: 'Home', href: '#home' },
+            { label: 'Cars', href: '#cars' },
+            { label: 'Promotions', href: '#promotions' },
+            { label: 'Customers', href: '#customers' },
+            { label: 'Questions', href: '#questions' }
+        ]
+    }
+]
+
+const desktopItems = [
+    { label: 'Home', href: '#home' },
+    { label: 'Cars', href: '#cars' },
+    { label: 'Promotions', href: '#promotions' },
+    { label: 'Customers', href: '#customers' },
+    { label: 'Questions', href: '#questions' }
+]
+</script>
+
+
 <template>
-    <nav class="navbar">
-        <NuxtLink to="/">
-            Home
-        </NuxtLink>
-        <NuxtLink to="/cars">
-            Rent a car
-        </NuxtLink>
-        <NuxtLink to="/vans">
-            Rent a van
-        </NuxtLink>
-        <NuxtLink to="/about">
-            About
-        </NuxtLink>
-    </nav>
+    <UNavigationMenu :items="mobileItems" :ui="{ childList: 'flex flex-col items-center navbar' }" trailing-icon="false"
+        class="md:hidden flex justify-end mt-2" />
+
+    <UNavigationMenu :items="desktopItems" class="hidden md:flex justify-center gap-6 mt-2" trailing-icon="false" :ui="{
+        list: 'flex gap-6',
+        item: 'text-sm font-medium hover:text-primary transition'
+    }" />
+
 </template>
