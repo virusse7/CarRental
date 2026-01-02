fb28f1b8c03ce22b6575d76bacde0a851fe919ef
Daniel Wieczorek <danielwieczorek901@gmail.com>
2026-01-02 16:20:12 +0100

    feat(app): added title and organized template

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,20 @@
 <script setup>
 import Navbar from './components/ui/Navbar.vue';
 import Footer from './components/ui/Footer.vue';
+
+useHead({
+    titleTemplate: 'Car Rental'
+})
 </script>
 
 <template>
-    <div>
-        <Navbar />
-        <main>
-            <NuxtPage />
-        </main>
-        <Footer />
-    </div>
+    <UApp>
+        <div>
+            <Navbar />
+            <main>
+                <NuxtPage />
+            </main>
+            <Footer />
+        </div>
+    </UApp>
 </template>
